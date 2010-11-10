6c32f96c479ad7562bde2ae07bc344a11e26b6a8
amolimac <amolrkon@gmail.com>
2010-11-10 12:18:36 +0600

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @title = " home"
   end
 
   def contact
+    @title = " Contact" 
   end
-
+  def about
+    @title = " About"
+   end
 end
